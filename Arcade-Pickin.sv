2344f836673d28e3d56148e83a7c575fa0bbb015
Alan Steremberg <alan.steremberg@gmail.com>
2020-01-15 08:18:10 -0800

    new release

--- Arcade-Pickin.sv
+++ Arcade-Pickin.sv
@@ -100,8 +100,8 @@ assign HDMI_ARY = status[1] ? 8'd9  : status[2] ? 8'd3 : 8'd4;
 `include "build_id.v" 
 localparam CONF_STR = {
 	"A.PICKIN;;",
-	"O1,Aspect Ratio,Original,Wide;",
-	"O2,Orientation,Vert,Horz;",
+	"H0O1,Aspect Ratio,Original,Wide;",
+	"H0O2,Orientation,Vert,Horz;",
 	"O35,Scandoubler Fx,None,HQ2x,CRT 25%,CRT 50%,CRT 75%;",
 	"-;",
 	"O89,Lives,2,3,4,5;",
@@ -111,7 +111,8 @@ localparam CONF_STR = {
 	//"ODE,Difficulty,Easy,Medium,Hard,Hardest;",	
 	"-;",
 	"R0,Reset;",
-	"J1,Fire,Start 1P,Start 2P;",
+	"J1,Fire,Start 1P,Start 2P,Coin;",
+	"jn,A,Start,Select,R;",
 	"V,v2",`BUILD_DATE
 };
 /*
@@ -148,6 +149,7 @@ pll pll
 wire [31:0] status;
 wire  [1:0] buttons;
 wire        forced_scandoubler;
+wire        direct_video;
 
 wire        ioctl_download;
 wire        ioctl_wr;
@@ -171,8 +173,10 @@ hps_io #(.STRLEN($size(CONF_STR)>>3)) hps_io
 
 	.buttons(buttons),
 	.status(status),
+	.status_menumask(direct_video),
 	.forced_scandoubler(forced_scandoubler),
 	.gamma_bus(gamma_bus),
+	.direct_video(direct_video),
 
 	.ioctl_download(ioctl_download),
 	.ioctl_wr(ioctl_wr),
@@ -199,12 +203,9 @@ always @(posedge clk_sys) begin
 			'h029: btn_fire        <= pressed; // space
 			'h014: btn_fire        <= pressed; // ctrl
 
-			'h005: btn_one_player  <= pressed; // F1
-			'h006: btn_two_players <= pressed; // F2
+			'h005: btn_start_1    <= pressed; // F1
+			'h006: btn_start_2    <= pressed; // F2
 			// JPAC/IPAC/MAME Style Codes
-
-			'h005: btn_one_player  <= pressed; // F1
-			'h006: btn_two_players <= pressed; // F2
 			'h016: btn_start_1     <= pressed; // 1
 			'h01E: btn_start_2     <= pressed; // 2
 			'h02E: btn_coin_1      <= pressed; // 5
@@ -224,8 +225,6 @@ reg btn_down  = 0;
 reg btn_right = 0;
 reg btn_left  = 0;
 reg btn_fire  = 0;
-reg btn_one_player  = 0;
-reg btn_two_players = 0;
 
 reg btn_start_1=0;
 reg btn_start_2=0;
@@ -240,21 +239,21 @@ reg btn_test=0;
 
 
 
-wire m_up     = status[2] ? btn_left  | joy[1] : btn_up    | joy[3];
-wire m_down   = status[2] ? btn_right | joy[0] : btn_down  | joy[2];
-wire m_left   = status[2] ? btn_down  | joy[2] : btn_left  | joy[1];
-wire m_right  = status[2] ? btn_up    | joy[3] : btn_right | joy[0];
+wire m_up     = btn_up    | joy[3];
+wire m_down   = btn_down  | joy[2];
+wire m_left   = btn_left  | joy[1];
+wire m_right  = btn_right | joy[0];
 wire m_fire   = btn_fire | joy[4];
 
-wire m_up_2     = status[2] ? btn_left_2  | joy[1] : btn_up_2    | joy[3];
-wire m_down_2   = status[2] ? btn_right_2 | joy[0] : btn_down_2  | joy[2];
-wire m_left_2   = status[2] ? btn_down_2  | joy[2] : btn_left_2  | joy[1];
-wire m_right_2  = status[2] ? btn_up_2    | joy[3] : btn_right_2 | joy[0];
+wire m_up_2     = btn_up_2    | joy[3];
+wire m_down_2   = btn_down_2  | joy[2];
+wire m_left_2   = btn_left_2  | joy[1];
+wire m_right_2  = btn_right_2 | joy[0];
 wire m_fire_2  = btn_fire_2|joy[4];
 
-wire m_start1 = btn_one_player  | joy[5];
-wire m_start2 = btn_two_players | joy[6];
-wire m_coin   = m_start1 | m_start2;
+wire m_start1 = btn_start_1 | joy[5];
+wire m_start2 = btn_start_2 | joy[6];
+wire m_coin   = btn_coin_1 | joy[7];
 
 wire hblank, vblank;
 //wire ce_vid = clk_sys;
@@ -270,13 +269,12 @@ always @(posedge clk_48m) begin
         div <= div + 1'd1;
         ce_pix <= !div;
 end
-
-arcade_rotate_fx #(512,224,8,0) arcade_video
+wire no_rotate = status[2] | direct_video;
+arcade_video #(512,224,8) arcade_video
 (
         .*,
 
         .clk_video(clk_48m),
-        //.ce_pix(ce_vid),
 
         .RGB_in({r,g,b}),
         .HBlank(hblank),
@@ -285,10 +283,8 @@ arcade_rotate_fx #(512,224,8,0) arcade_video
         .VSync(vs),
 
         .fx(status[5:3]),
-        .no_rotate(status[2])
+        .rotate_ccw(0)
 );
-//screen_rotate #(256,224,8) screen_rotate
-
 
 wire [12:0] audio;
 assign AUDIO_L = {audio, 3'b000};
@@ -319,9 +315,9 @@ Pickin Pickin
 	.video_vs(vs),
 	.video_hblank(hblank),
 	.video_vblank(vblank),
-	.start2(m_start2|btn_start_2),
-	.start1(m_start1|btn_start_1),
-	.coin1(m_coin|btn_coin_1|btn_coin_2),
+	.start2(m_start2),
+	.start1(m_start1),
+	.coin1(m_coin|btn_coin_2),
   
 	.fire1(m_fire),
 	.right1(m_right),
